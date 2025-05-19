2b1e1cfeab82fdbcdc3d42f576df7ec9adc4b67d
Anima-ui <falko.artem.w@gmail.com>
2025-05-19 20:05:19 +0300

    added WebSocket config and ChatMessage class

---
+++ webSocketChat/src/main/java/com/anima/webSocketChat/config/WebSocketConfiguration.java
@@ -0,0 +1,21 @@
+package com.anima.webSocketChat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic", "/queue");
+    }
+}
---
+++ webSocketChat/src/main/java/com/anima/webSocketChat/model/enums/MessageType.java
@@ -0,0 +1,7 @@
+package com.anima.webSocketChat.model.enums;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
